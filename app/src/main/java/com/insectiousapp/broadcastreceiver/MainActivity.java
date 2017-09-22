b607584d11a7eceb6401aa339ea20fe501b2ab32
chetanmann95@gmail.com <chetanmann95@gmail.com>
2017-09-22 11:40:47 +0530

    added dynamic permissions

--- app/src/main/java/com/insectiousapp/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/insectiousapp/broadcastreceiver/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,35 +15,36 @@ import android.telephony.PhoneStateListener;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
-    Button btnDMOn, btnDMOff;
     IncomingCallReceiver incomingCallReceiver;
     CheckBox cbSend;
     String MyPREFERENCES="spmain";
     String CHOICE="choice";
     SharedPreferences sharedpreferences;
+    ToggleButton btnDriverMode;
+    boolean permissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        permissionGranted=isPermissionGranted();
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-
         incomingCallReceiver=new IncomingCallReceiver();
-        btnDMOn=(Button)findViewById(R.id.btRegister);
-        btnDMOff=(Button)findViewById(R.id.btUnregister);
+        btnDriverMode=(ToggleButton)findViewById(R.id.btnDriverMode);
         cbSend=(CheckBox) findViewById(R.id.cbSendMessage);
-
-        btnDMOn.setOnClickListener(this);
-        btnDMOff.setOnClickListener(this);
+        btnDriverMode.setOnCheckedChangeListener(this);
 
         cbSend.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
         {
@@ -75,35 +77,100 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId())
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+        if(isPermissionGranted()) {
+            //check if all permissions are granted
+            //if all permissions are granted then only allow user to proceed
+            if (compoundButton.getId() == R.id.btnDriverMode)
+
+            {
+                if (b == true) {
+                    //driver mode on
+                    registerReceiver(incomingCallReceiver, new IntentFilter("android.intent.action.PHONE_STATE"));
+                    btnDriverMode.setTextOn("Driver Mode On");
+                    Toast.makeText(this, "Driver mode On !", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //driver mode off
+
+                    unregisterReceiver(incomingCallReceiver);
+                    btnDriverMode.setTextOff("Driver Mode Off !");
+                    Toast.makeText(this, "Driver mode Off !!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getTitle().equals("item1"))
         {
-            case R.id.btRegister:
+            Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getTitle().equals("item2"))
+        {
+            Toast.makeText(this, "Item 2", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
-                registerReceiver(incomingCallReceiver, new IntentFilter("android.intent.action.PHONE_STATE"));
-                Toast.makeText(this, "Driver mode On !", Toast.LENGTH_SHORT).show();
+    public  boolean isPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
 
-//                TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-//                tm.listen(callStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
+                    == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.SEND_SMS)
+                    == PackageManager.PERMISSION_GRANTED&& checkSelfPermission(Manifest.permission.MODIFY_AUDIO_SETTINGS)
+                    == PackageManager.PERMISSION_GRANTED&& checkSelfPermission(Manifest.permission.VIBRATE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v("TAG","Permission is granted");
+                return true;
+            } else {
 
-                break;
-
-            case R.id.btUnregister:
-
-                unregisterReceiver(incomingCallReceiver);
-                Toast.makeText(this, "Driver mode Off !!", Toast.LENGTH_SHORT).show();
-
-//                String strPhone = "9574110030";
-//                String strMessage = "Assignment done baby !";
-//
-//                SmsManager sms = SmsManager.getDefault();
-//
-//                sms.sendTextMessage(strPhone, null, strMessage, null, null);
-//
-//                Toast.makeText(this, "Sent Message to : "+strPhone, Toast.LENGTH_SHORT).show();
-
-                break;
+                Log.v("TAG","Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.SEND_SMS,
+                        Manifest.permission.MODIFY_AUDIO_SETTINGS, Manifest.permission.VIBRATE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("TAG","Permission is granted");
+            return true;
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+
+            case 1: {
+
+                if (grantResults.length > 3
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED&& grantResults[1] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[2] == PackageManager.PERMISSION_GRANTED&& grantResults[3] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_SHORT).show();
+                 //   call_action();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
 }
